fe4e3e115b7fa671a66617697b7c59e112c4080f
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-23 16:40:59 -0700

    Add strategy for local login

--- config/passport.js
+++ config/passport.js
@@ -26,6 +26,42 @@ module.exports = function(passport) {
     });
   });
 
+
+  // ===========================================================================
+  // LOCAL LOGIN ==============================================================
+  // ===========================================================================
+  // we are using named strategies since we have one for login and one for signup
+  // by default, if there was no name, it would just be called 'local'
+
+  passport.use('local-login', new LocalStrategy({
+      // by default local strategy uses usernameand password, we will override it to use password
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+    function(req, email, password, done) { // callback with email and password
+      // find a user whose email is same as the forms email
+      // we are checking to see if the user is trying to login already exists
+      User.findOne({
+        'local.email': email
+      }, function(err, user) {
+        // if there are any errors, return the error before anything else
+        if (err)
+          return done(err);
+
+        // if no user found, return the messages
+        if (!user)
+          return done(null, false, req.flash('loginMessage', 'No user found.'));
+        // if there is a user foun but tpassword is wrong
+        if (!user.validPassword(password))
+          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password'));
+
+        // all is well, return successfull user
+        return done(null, user);
+
+      });
+    }));
+
   // ===========================================================================
   // LOCAL SIGNUP ==============================================================
   // ===========================================================================
