3ab823be5235db6e82dd0baa1f4be476726ace3a
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-22 23:48:15 -0700

    Logic to generate hash for password

--- app/models/user.js
+++ app/models/user.js
@@ -29,3 +29,15 @@ var userSchema = mongoose.Schema({
   }
 
 });
+
+// methods
+// generate a hash
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
